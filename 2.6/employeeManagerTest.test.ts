f4cf6f27532017a8386c361e815f72a24f766a69
Courtney Vennard <courtney.vennard@etapro.com>
2023-11-14 16:29:43 -0500

    Homework2.6

--- 2.6/employeeManagerTest.test.ts
+++ 2.6/employeeManagerTest.test.ts
@@ -9,28 +9,43 @@ import {Builder,By, Capabilities, until, WebDriver, } from "selenium-webdriver";
       driver: WebDriver;
       url: string = "https://devmountain-qa.github.io/employee-manager/1.2_Version/index.html";
         //FILL OUT LOCATORS CONSTRUCTOR AND METHODS IN ORDER TO PASS THE TEST
-  }
+        header: By=By.className('titleText');
+        addEmployee: By=By.name('addEmployee');
+        newEmployee: By=By.xpath('(//li[@class="listText"])[11]');
+        nameInput: By=By.name('nameEntry');
+        phoneInput: By=By.name('phoneEntry');
+        titleInput: By=By.name('titleEntry');
+
+        constructor(driver: WebDriver){
+            this.driver=driver;
+        }
+        async navigate() {
+            await this.driver.get(this.url);
+            await this.driver.wait(until.elementLocated(this.header));  
+  };
+};
+  const emPage=new employeePage(driver);
 
   describe("Employee Manger Test", () => {
       beforeEach(async () => {
-          await employeePage.navigate();
-      })
+          await emPage.navigate();
+      });
       afterAll(async () => {
-          await driver.quit()
-      })
+          await emPage.driver.quit();
+      });
       test("adding an employee", async () => {
-          await driver.wait(until.elementLocated(emPage.header))
-          await driver.findElement(emPage.addEmployee).click()
-          await driver.findElement(emPage.newEmployee).click()
-          await driver.findElement(emPage.nameInput).click()
-          await driver.findElement(emPage.nameInput).clear()
-          await driver.findElement(emPage.nameInput).sendKeys("Change this")
-          await driver.findElement(emPage.phoneInput).click()
-          await driver.findElement(emPage.phoneInput).clear()
-          await driver.findElement(emPage.phoneInput).sendKeys("Change this")
-          await driver.findElement(emPage.titleInput).click()
-          await driver.findElement(emPage.titleInput).clear()
-          await driver.findElement(emPage.titleInput).sendKeys("Change this")
-  })
-
+          await driver.wait(until.elementLocated(emPage.header));
+          await driver.findElement(emPage.addEmployee).click();
+          await driver.findElement(emPage.newEmployee).click();
+          await driver.findElement(emPage.nameInput).click();
+          await driver.findElement(emPage.nameInput).clear();
+          await driver.findElement(emPage.nameInput).sendKeys("Test Name");
+          await driver.findElement(emPage.phoneInput).click();
+          await driver.findElement(emPage.phoneInput).clear();
+          await driver.findElement(emPage.phoneInput).sendKeys(8145693247);
+          await driver.findElement(emPage.titleInput).click();
+          await driver.findElement(emPage.titleInput).clear();
+          await driver.findElement(emPage.titleInput).sendKeys("Test Job");
+  });
+  });
   /* this is a commment */
